c282a171f5b7e2fc07a6a168a2708e7aeb206884
sousuke0422 <sousuke20xx@gmail.com>
2020-04-24 18:04:05 +0900

    1.0.0

---
+++ src/main/kotlin/xyz/akirin/aoi/main.kt
@@ -0,0 +1,15 @@
+package xyz.akirin.aoi
+
+import cpw.mods.fml.common.Mod
+
+
+@Mod(modid = main.ID, name = main.Name, version = main.Version, modLanguage = main.Lang)
+class main {
+
+    companion object {
+        const val ID = "aoi"
+        internal const val Name = "X Lib"
+        internal const val Version = "core:1.0.0, codename:aoi"
+        internal const val Lang = "kotlin"
+    }
+}
